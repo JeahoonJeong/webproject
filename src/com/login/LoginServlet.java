c3fe0e93cef2a2aaf30873210b2732c6866ddd8e
Unknown <itra48@naver.com>
2018-08-14 16:50:25 +0900

    login 페이지

--- src/com/login/LoginServlet.java
+++ src/com/login/LoginServlet.java
@@ -38,6 +38,8 @@ public class LoginServlet extends HttpServlet{
 		
 		Connection conn = DBCPConn.getConnection();
 		
+		MemberDAO dao = new MemberDAO(conn);
+		
 		String url;
 		
 		String uri = req.getRequestURI();
@@ -47,6 +49,41 @@ public class LoginServlet extends HttpServlet{
 			
 			url = "/loginPage/login.jsp";
 			forward(req, resp, url);
+			
+		}else if(uri.indexOf("login_ok.do")!=-1){
+			
+			String user_id = req.getParameter("user_id");
+			String user_pwd = req.getParameter("user_pwd");
+			
+			MemberDTO dto = dao.getReadMemberData(user_id);
+			
+			if(dto==null || !(dto.getUser_id().equals(user_id))){
+				
+				req.setAttribute("message", "아이디를 다시 확인하세요!");
+				
+				url = "/loginPage/login.jsp";
+				forward(req, resp, url);
+				
+				return;
+				
+			}else if(!(dto.getUser_pwd().equals(user_pwd))){
+				
+				req.setAttribute("message", "비밀번호를 다시 확인하세요!");
+				
+				url = "/loginPage/login.jsp";
+				forward(req, resp, url);
+				
+				return;
+			}
+			
+			HttpSession session = req.getSession(true);
+			
+			session.setAttribute("member", dto);
+			
+			url = cp + "/Mypage/mypageMain.do";
+			
+			resp.sendRedirect(url);
+
 			
 		}
 		
